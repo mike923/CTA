8d437803336d43d67ddd7298841d9e2dd4801b4f
MikePursuit <michaelamparo@pursuit.org>
2020-02-09 08:18:01 -0500

    initialized frontend npx create react app

--- server/db/queries/users.js
+++ server/db/queries/users.js
@@ -3,9 +3,9 @@ const db = require('../config')
 const getAllUsers = async () => await db.any('SELECT id, username, avatar_url FROM users')
 
 const addNewUser = async (user) => await db.one(`
-	INSERT INTO users(username, password_digest)
-	VALUES($/username/, $/password/)
-	RETURNING id, username
+	INSERT INTO users(username, password_digest, avatar_url)
+	VALUES($/username/, $/password/, $/avatar_url/)
+	RETURNING id, username, avatar_url
 `, user)
 
 const getUserByID = async (id) => await db.oneOrNone('SELECT id, username, avatar_url FROM users WHERE id = $1', [id])
