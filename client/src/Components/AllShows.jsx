379d92daedd4e63855ad97c285d12f87aefb09ab
MikePursuit <michaelamparo@pursuit.org>
2020-02-12 23:47:20 -0500

    working on css for shows page

--- client/src/Components/AllShows.jsx
+++ client/src/Components/AllShows.jsx
@@ -2,21 +2,27 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 const AllShows = ({shows}) => (
-    <ul>
+    <div className="container row">
         {shows.map(({title, genre_name, img_url, viewers}, i) => (
-            <li key={i}>
-                <img src={img_url} alt={title} height="200px"/>
-                <h3>{title}</h3>
-                <h4>{genre_name}</h4>
-                Being watched by: 
-                <ol>
-                    {viewers.map(({username, show_id, user_id}, i) => (
-                        <li key={i}><Link to={`shows/${show_id}/user/${user_id}`}>{username}</Link></li>
-                    ))}
-                </ol>
-            </li>
+            <div className="col s4 m4" key={i} style={{overflow: 'auto'}}>
+                <h2 className="header">{title}</h2>
+                <div className="card ">
+                    <div className="card-image">
+                        <img src={img_url} alt={title} />
+                    </div>
+                    <div className="card-content" style={{overflow: 'auto'}}>
+                        <h4>{genre_name}</h4>
+                        <ul>
+                            <li key={0}><h5>Being watched by</h5></li>
+                            {viewers.map(({username, show_id, user_id}, i) => (
+                                <li key={i}><Link to={`shows/${show_id}/user/${user_id}`}>{username}</Link></li>
+                                ))}
+                        </ul>
+                    </div>
+                </div>
+            </div>
         ))}
-    </ul>
+    </div>
 )
 
 export default AllShows
